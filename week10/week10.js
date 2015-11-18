abc9b5fdc5361a5b07344220232d367bc80733f1
Zulukas <1996nissan200sx@gmail.com>
2015-11-18 12:43:07 -0700

    updated week 10 git

--- week10/week10.js
+++ week10/week10.js
@@ -1,52 +1,87 @@
 function getCountry(input) {
 	var index = input.selectedIndex;
 	var val = input[index].text;
-	var parse = document.forms["countrySelect"];
-	var parseElements = parse.elements;
-	var citiesSel = parseElements["cities"];	
 	var country = val.toLowerCase();
 
+	//var file = "http://157.201.194.254/~cs213/" + country + ".txt";
+	var file = country + ".txt";
+
 	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
+	xhttp.onreadystatechange = function() {  
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			document.getElementById("blah").innerHTML = xhttp.responseText;
-		}
-	};
+  			fillTable("CITIES", xhttp.responseText);
+    }
+  };
+  
+  xhttp.open("GET", file, true);
+  xhttp.send();
 
-	xhttp.open("GET", "http://157.201.194.254/~cs213/" + country + ".txt", true);
-	xhttp.send();
-
-	// alert ("country!");
-
-	// if (country !== "USA" || country !== "Mexico" ||
-	// 	country !== "Canada" || country !== "Russia") {
-	// 	return;
-	// }
-
-	// var directory = "http://157.201.194.254/~cs213";
-
-	// Http.get({
-	// 	url: "http://157.201.194.254/~cs213" + country + ".txt",
-	// 	callback: fillCities,
-	// 	cache: Http.Cache.Get
-	// }, [citiesSel]);
-
-	// return;
 }
 
-// function fillCities(xmlreply, citiesElement) {
-// 	if (xmlreply.status == Http.Status.OK) {
-// 		var citiesResponse = xmlreply.responseText;
-// 		var cities = citiesResponse.split("|");
-// 		citiesElement.length = 1;
-// 		citiesElement.length = cities.length;
+function fillTable(table_ID, data) {
+	// console.log(data);
+	//Get the table in question
+	var table = document.getElementById(table_ID);
 
-// 		for (var i = 0; i < cities.length; i++) {
-// 			citiesElement[i].text = cities[i];
-// 		}
-// 	}
-// 	else
-// 	{
-// 		"Unable to handle the Ajax call.");
-// 	}
-// }
+	//Split the data into bite-size pieces
+	// var parts = data.replace(/\n/g, " ").split(" ");
+	var parts = data.replace(/(\s\s+|\n)/g, " ").split(" ");
+
+	console.log(parts);
+
+	//Number of rows in our current table
+	var length = table.rows.length;
+
+	//Clear the old data
+	for (var i = 1; i < length; i++) {
+		table.deleteRow(1);
+	}
+
+	//Initialize data arrays
+	var fieldA = [];
+	var indexA = 0;
+	var fieldB = [];
+	var indexB = 0;
+
+	var enterIntoFieldA = true;
+
+
+
+	//Populate the data arrays
+	for (var i = 0; i < parts.length; i++) {
+		if (enterIntoFieldA) {
+			fieldA[indexA] += parts[i];
+
+			if (parts[i + 1])
+		}
+		else {
+
+		}
+	}
+
+	// for (var i = 0; i < parts.length; i++) {
+	// 	if (i % 2 == 0) {
+	// 		fieldA.push(parts[i]);
+	// 	}
+	// 	else {
+	// 		fieldB.push(parts[i]);
+	// 	}
+	// }
+
+	 console.log(fieldA);
+	 console.log(fieldB);
+
+	// console.log("Num cities: " + cities.length);
+	// console.log("Num pops: " + populations.length);
+
+	//Populate the table
+	for (var i = 0; i < fieldA.length; i++) {
+		var row = table.insertRow(i + 1);
+
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+
+		cell1.innerHTML = fieldA[i];
+		cell2.innerHTML = fieldB[i];
+	}
+}
